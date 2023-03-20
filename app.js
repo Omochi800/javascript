b825da874f22ff065ee7a855697fb86f9015a917
HikaruSato <110364092+Omochi800@users.noreply.github.com>
2023-03-20 09:38:24 +0000

    js1-3

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
-let max = 100;
-let num = 1;
-let count = 0;
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-while(num < max){
-  num = num*2;
-  count = count + 1;
+let js_hand = getJShand();
+
+let judge = winLose(user_hand,js_hand);
+
+alert('あなたの選んだ手は'+user_hand+'です。');
+
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random()*3);
+  let hand_name;
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  }else if(js_hand_num == 1){
+    hand_name ="チョキ";
+  }else if(js_hand_num == 2){
+    hand_name ="パー";
+  }
+
+  return hand_name;
 }
 
-alert('2を掛け続けて' + max + 'を越えるのに必要だった回数は' + count +'回です');
+
+function winLose(user,js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr="あいこ";
+    }else if(js=="チョキ"){
+      winLoseStr="勝ち";
+    }else if(js=="パー"){
+      winLoseStr="負け";
+    }
+  }else if(user=="チョキ"){
+    if(js=="グー"){
+      winLoseStr="負け";
+    }else if(js=="チョキ"){
+      winLoseStr="あいこ";
+    }else if(js=="パー"){
+      winLoseStr="勝ち";
+    }
+  }else if(user=="パー"){
+    if(js=="グー"){
+      winLoseStr="勝ち";
+    }else if(js=="チョキ"){
+      winLoseStr="負け";
+    }else if(js=="パー"){
+      winLoseStr="あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
